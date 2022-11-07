df5e1f1663b4fa470adde8dd4386224ce8611178
ChandrasekarNakamoto <chandrasekar2018105518@gmail.com>
2022-11-07 20:13:14 +0530

    commit

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,11 +7,17 @@ export default function Card(props){
         <div className='firstcard'>
         <img src={props.imageUrl}/>
         </div>
+        <div>
         <div className='seccard'>
             <i><TfiLocationPin style={style}/></i>
             <h4 className='loc'>{props.location}</h4>
             <a className='map' href={props.googleMapsUrl} target="_blank">View on google maps</a>
+            </div>
+            <h2>{props.title}</h2>
+        <h4>{props.startDate}-{props.endDate}</h4>
+        <p>{props.description}</p>
         </div>
+        
     </section>
     )
 }
